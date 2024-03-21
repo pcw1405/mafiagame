4f606edbe12bb6db897d0a98fae4ec34cb27e4f5
chungwon <pcw14057@gmail.com>
2024-03-21 19:52:34 +0900

    로그인을 하고 로그인의 이메일을 통해 채팅닉네임을 정한다

--- src/main/java/com/example/mafiagame/controller/ChatController.java
+++ src/main/java/com/example/mafiagame/controller/ChatController.java
@@ -1,8 +1,12 @@
 package com.example.mafiagame.controller;
 
+import com.example.mafiagame.dto.MemberFormDto;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Log4j2
@@ -12,6 +16,18 @@ public class ChatController {
     public String chatGET(){
         log.info("@ChatController.chat GET()");
         return "chater";
+    }
+
+    @GetMapping("/chat/user")
+    @ResponseBody
+    public String getUser() {
+        log.info("getUserInfo start");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = null;
+        if (auth != null && auth.isAuthenticated()) {
+            name = auth.getName();
+        }
+        return name;
     }
 }
 
