05cf1f202b9f58f61bed40a959f4503b48a98663
chungwon <pcw14057@gmail.com>
2024-04-20 02:29:32 +0900

    상대방이 게임수락을 요청하면 mysql에 게임생성하고 게임정보가 입력된다

--- src/main/java/com/example/mafiagame/service/ChatService.java
+++ src/main/java/com/example/mafiagame/service/ChatService.java
@@ -16,9 +16,8 @@ public class ChatService {
     private final ChannelTopic channelTopic;
     private final RedisTemplate redisTemplate;
     private final ChatRoomRepository chatRoomRepository;
+    private final MiniGameRepository miniGameRepository;
 
-
-    private MiniGameRepository miniGameRepository;
 //    destination 정보에서 roomId정보 추출
     public String getRoomId(String destination){
         int lastIndex =destination.lastIndexOf('/');
@@ -30,13 +29,20 @@ public class ChatService {
 
 //    오버라이딩
     public void sendChatMessage(ChatMessage chatMessage, String gameMaker) {
+        String requester =gameMaker;
         // 두 번째 인자를 활용한 새로운 메서드 구현 내용...
         chatMessage.setUserCount(chatRoomRepository.getUserCount(chatMessage.getRoomId()));
         if (chatMessage.getType().equals(ChatMessage.MessageType.GAME_REQUEST_ACCEPT)){
             chatMessage.setMessage(chatMessage.getSender()+"님이 게임요청을 수락했습니다");
+
+
             MiniGame game = new MiniGame();
-            game.setPlayer1(chatMessage.getSender());
+            game.setPlayer2(chatMessage.getSender());
             game.setPlayer1(gameMaker);
+            miniGameRepository.save(game);
+            chatMessage.setSender("[시스템]");
+        }else if(chatMessage.getType().equals(ChatMessage.MessageType.GAME_REQUEST)){
+            chatMessage.setMessage(chatMessage.getSender()+"님의 게임요청");
             chatMessage.setSender("[시스템]");
         }
         //GAME_REQUEST_ACCEPT
@@ -53,10 +59,6 @@ public class ChatService {
         }else if(ChatMessage.MessageType.QUIT.equals(chatMessage.getType())){
             chatMessage.setMessage(chatMessage.getSender()+"님이 방에서 나갔습니다.");
             chatMessage.setSender("[알림]");
-        }else  if (chatMessage.getType().equals(ChatMessage.MessageType.GAME_REQUEST)) {
-            chatMessage.setMessage(chatMessage.getSender()+"님의 게임요청");
-            chatMessage.setSender("[시스템]");
-
         }else if (chatMessage.getType().equals(ChatMessage.MessageType.GAME_REQUEST_REJECT)){
             chatMessage.setMessage(chatMessage.getSender()+"님이 게임요청을 거절했습니다");
             chatMessage.setSender("[시스템]");
